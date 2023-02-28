49829ef225ba6a3751e80da4b9443783a8ed7769
Adam <85541373+creepicrawli@users.noreply.github.com>
2023-02-28 23:54:24 +0200

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey = '744E7E894F03431EB19C007970095EBE';
+
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`
+                }
+            }).then(response => {
+                return response.json();
+            }).then(jsonResponse => {
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map(business => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories[0].title,
+                            rating: business.rating,
+                            reviewCount: business.review_count,
+
+                        }
+                    });
+                }
+            })
+    }
+
+};
+
+export default Yelp;
